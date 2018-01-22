b7e880c8775e2d3e3e9047fc898e3c245e7c6fdb
Daniel Moss <dmoss@stratoswp.com>
2018-01-22 11:51:07 -0500

    initial commit

---
+++ bamazonCustomer.js
@@ -0,0 +1,28 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+// variable to store connection to products database
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'testuser',
+  password : 'Welcome123',
+  database : 'bamazon'
+});
+
+// connection.connect();
+
+// connection.query("SELECT * FROM products", function (err, results) {
+//     if (err) throw err;
+//     console.log(results);
+// })
+
+// connection.end();
+
+conection.connect(function (err) { 
+    if (err) throw err;
+    start();
+ })
+
+ function start() {
+     // query the database for all items available
+ }
